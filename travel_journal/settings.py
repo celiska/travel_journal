5fb2ff35ef60fa1dfa64459568daa93c3aee783b
Adam Gomez <aditoo@seznam.cz>
2024-09-23 09:31:39 +0200

    Remove secret key from settings

--- travel_journal/settings.py
+++ travel_journal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jo-@+r$de=jekxui18czv!j_^bvzu7z%7rm-t8#gjqb6!*2up1'
+from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

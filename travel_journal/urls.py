9f61e39c774e83f2b33e8d3e2c01d071d59641a3
celiska <eliska.ciprova@seznam.cz>
2024-09-25 09:24:33 +0200

    UNDONE: entries page

--- travel_journal/urls.py
+++ travel_journal/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from viewer.views import home, trips
+from travel_journal import settings
+from viewer.views import home, EntriesList
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
 
     path('', home, name='home'),
 
-    path('trips/', trips, name='trips' ),
-]
+    path('entries/', EntriesList.as_view(), name='entries' ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- viewer/views.py
+++ viewer/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from viewer.models import Entry
+
 
 def home(request):
     return render(request, "home.html")
 
-def trips(request):
-    return render(request, "trips.html")
+class EntriesList(ListView):
+    template_name = "entries.html"
+    model = Entry
+    context_object_name = 'entries'

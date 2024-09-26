a7b7649c379059d0f28594e00178532765fe0366
Adam Gomez <aditoo@seznam.cz>
2024-09-26 14:53:14 +0200

    Create Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    class Meta:
+        ordering = ['user__username']
+
+    def __str__(self):
+        return f"{self.user}"
+
+    def __repr__(self):
+        return f"Profile(user={self.user})"

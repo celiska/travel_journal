14516bcc0c8b008157d7c7bef09ff351a633f7ac
Adam Gomez <aditoo@seznam.cz>
2024-10-04 14:05:34 +0200

    Login user automatically after signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -9,9 +10,18 @@ from .forms import SignUpForm
 
 # Create your views here.
 class SignUpView(CreateView):
-    template_name = 'form.html'
+    template_name = 'registration/signup.html'
     form_class = SignUpForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
+
+    # Login user automatically after signup
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return HttpResponseRedirect(self.success_url)
 
 @login_required
 def user_logout(request):

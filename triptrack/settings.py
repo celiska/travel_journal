8bd1d33defc9c8ea72bbab30632ea71883b0ab12
celiska <eliska.ciprova@seznam.cz>
2024-12-11 21:00:29 +0100

    media&static changes

--- triptrack/settings.py
+++ triptrack/settings.py
@@ -125,7 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
